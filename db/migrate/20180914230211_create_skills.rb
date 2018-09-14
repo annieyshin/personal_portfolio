1b7178d75eaa601f65095664abaed3f2d8461a49
Annie Shin <annieyshin@gmail.com>
2018-09-14 16:30:49 -0700

    update dbs

--- db/migrate/20180914230211_create_skills.rb
+++ db/migrate/20180914230211_create_skills.rb
@@ -1,6 +1,11 @@
 class CreateSkills < ActiveRecord::Migration[5.2]
   def change
     create_table :skills do |t|
+      t.column :name, :string
+      t.column :description, :string
+      t.column :project_id, :integer
+
+       t.timestamps
     end
   end
 end
